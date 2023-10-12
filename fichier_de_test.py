a188164e47de24ea93fe3195ecb0c3bf6a23e6c1
Ledeveulopper <147757343+Ledeveulopper@users.noreply.github.com>
2023-10-12 21:43:24 +0200

    Add files via upload

---
+++ fichier_de_test.py
@@ -0,0 +1,4 @@
+liste = ["Hamza", "Nüma", "Klévio", "Timour", "bilalkebab"]
+liste = "-".join(liste)
+
+print(liste)
---
+++ projet/calculatrice#1.py
@@ -0,0 +1,3 @@
+a=input("Choisisez un premier nombre: ")
+b=input("choisisez un second nombre: ")
+(f"Le résultat est {int(a) + int(b)}!!!!!!!!!")
